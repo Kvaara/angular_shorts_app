41dcde47ba46371fc7c117035427ef42157f6b68
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-12-31 02:06:39 +0200

    Started creating an ID System for the modal logic (still need to refactor a couple of components to use this new system). I created this ID System because of the Angular's automatic singleton pattern on services. The modal component is going to be reusable (i.e its content is going to be dynamic), which means that the singleton pattern will cause all of the modals to open. ID System will help in opening the correct one.

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,19 +1,33 @@
 import { Injectable } from '@angular/core';
 
+interface IModal {
+  id: string;
+  isHidden: boolean;
+};
+
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
-  isHidden: Boolean = true;
+  modals: IModal[] = [];
 
   constructor() { }
 
-  isModalOpen() {
-    return this.isHidden;
+  registerModal(id: string): void {
+    this.modals.push({
+      id,
+      isHidden: true,
+    });
   }
 
-  toggleModal($event?: Event) {
+  isModalOpen(id: string): boolean {
+    return !!this.modals.find((modal) => modal.id === id)?.isHidden;
+  }
+
+  toggleModal(id: string, $event?: Event): void {
     if ($event) $event.preventDefault();
-    this.isHidden = !this.isHidden;
+    
+    const modal = this.modals.find((modal) => modal.id === id);
+    if (modal) !modal.isHidden;
   }
 }
