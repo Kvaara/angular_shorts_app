8a5d131d96caf61aba50be2e8b7a9ae887124873
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2022-01-06 15:22:15 +0200

    Added input masking into the phone number field. The input masking is in the finnish phone number format (e.g xxx xxx xxxx). I also modified the error messages a bit.

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -8,9 +8,11 @@ import { FormControl } from '@angular/forms';
 })
 export class InputComponent implements OnInit {
   @Input() control: FormControl = new FormControl();
-  @Input() inputType: String = "text";
-  @Input() inputPlaceholder: String = "Lorem Ipsum";
-  @Input() inputId: String = "";
+  @Input() inputType: string = "text";
+  @Input() inputPlaceholder: string = "Lorem Ipsum";
+  @Input() inputId: string = "";
+  @Input() format: string = "";
+  
 
   constructor() { }
 
