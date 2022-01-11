7dfa483e06e6c416bf16eed00d1317212fdd80b5
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2022-01-11 11:58:48 +0200

    Configured Firebase to the shorts application. Installed the Angularfire SDK to make interacting with Firebase a little bit easier.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDCQn8JSGx3gaNbznNsNAP7GY-uN91znus",
+    authDomain: "angular-shorts-app.firebaseapp.com",
+    projectId: "angular-shorts-app",
+    storageBucket: "angular-shorts-app.appspot.com",
+    appId: "1:916915355676:web:6247d731060774adf39fd9"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDCQn8JSGx3gaNbznNsNAP7GY-uN91znus",
+    authDomain: "angular-shorts-app.firebaseapp.com",
+    projectId: "angular-shorts-app",
+    storageBucket: "angular-shorts-app.appspot.com",
+    appId: "1:916915355676:web:6247d731060774adf39fd9"
+  },
 };
 
 /*
