d65e5647ca141755b172787117bea12cf7b83ea4
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2022-01-20 14:50:31 +0200

    Implemented a sign out feature. Users can now sign out from the application. AngularFireAuth handles this by removing tokens automatically.

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AuthService } from '../services/auth.service';
 import { ModalService } from '../services/modal.service';
 
@@ -9,10 +10,16 @@ import { ModalService } from '../services/modal.service';
 })
 export class NavComponent implements OnInit {
 
-  constructor(public modal: ModalService, public auth: AuthService) { 
+  constructor(public modal: ModalService, public auth: AuthService, private afAuth: AngularFireAuth,) { 
   }
 
   ngOnInit(): void {
+  }
+
+  async signOut($event: Event) {
+    $event.preventDefault();
+
+    await this.afAuth.signOut();
   }
 
 }
