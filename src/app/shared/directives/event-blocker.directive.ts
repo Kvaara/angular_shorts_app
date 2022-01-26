5423764e61d9c97ae6f3bdc6f2b45a1eca3436aa
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2022-01-26 02:52:41 +0200

    Created a global custom directive with the intent of preventing the default behavior of 'drop' and 'dragover' events. The HostListener decorator proves to be very useful in this situation. The function its decorated on gets called whenever the provided eventName gets triggered (E.g drop/dragover). In addition, we can get the event object from the HostListener decorator.

--- src/app/shared/directives/event-blocker.directive.ts
+++ src/app/shared/directives/event-blocker.directive.ts
@@ -7,8 +7,8 @@ export class EventBlockerDirective {
   constructor() { }
 
   @HostListener("drop", ["$event"])
+  @HostListener("dragover", ["$event"])
   public handleEvent(event: Event) {
     event.preventDefault();
-    event.stopPropagation();
   }
 }
