047bee148a78fd61b6420bba7d454f38961ed21a
Jesse Karnavaara <karnavaara.jesse@gmail.com>
2022-02-21 06:10:47 +0200

    Configured the videoplayer with the video.js framework. The videoplayer now has some custom styling applied to it. I need to fix the weird aspect-ratio bug.

--- src/app/short/short.component.ts
+++ src/app/short/short.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import videojs from 'video.js';
 
 @Component({
   selector: 'app-short',
   templateUrl: './short.component.html',
-  styleUrls: ['./short.component.scss']
+  styleUrls: ['./short.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ShortComponent implements OnInit {
+  @ViewChild("videoPlayer", { static: true }) videoPlayer?: ElementRef;
    id = "";
+   videoJsPlayer?: videojs.Player;
 
   constructor(public activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.videoJsPlayer = videojs(this.videoPlayer?.nativeElement);
     this.activatedRoute.params.subscribe( (params: Params) => 
       this.id = params["id"]
     );
